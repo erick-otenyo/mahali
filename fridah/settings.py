335affd870ee71c41a7e020b8f3b0965888ec6b2
Erick Otenyo <otenyo.erick@gmail.com>
2017-08-28 22:13:13 +0300

    Update Django for whitenoise static

--- fridah/settings.py
+++ fridah/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!^--%1qp4klkqr0yvlnfg=d$&#=@_nkzp78_b0kfd1!52peo0v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mahalii.herokuapp.com']
 
 # Application definition
 
